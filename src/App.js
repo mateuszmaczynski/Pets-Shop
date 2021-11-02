6af8c968083cddd0caf6dacbb4266ab3e86f6bd4
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-11-02 21:31:51 +0100

    add index file in Components dictionary

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import './components/_base.scss';
+import {
+  Footer,
+  Header,
+  Main,
+  Navbar,
+  NavbarItem
+} from './components'
+import bird from './assets/icons/bird.gif';
+import cat from './assets/icons/cat.gif';
+import dog from './assets/icons/dog.gif';
+import rabbit from './assets/icons/rabbit.gif';
 
 function App() {
   return (
     <div className='container'>
+      <Header/>
+      <Navbar>
+        <NavbarItem text='Dog' icon={dog} />
+        <NavbarItem text='Cat' icon={cat} />
+        <NavbarItem text='Small Pet' icon={rabbit} />
+        <NavbarItem text='Bird' icon={bird} />
+      </Navbar>
+      <Main>
+        lorem ipsum
+      </Main>
+      <Footer/>
     </div>
   );
 }
