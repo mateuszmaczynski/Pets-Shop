ee007332585fb219a03d55ba58c0bddc61d23c24
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-11-02 21:33:09 +0100

    modify app.js - adding components to view

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Footer from './Footer';
+import Header from './Header';
+import Main from "./Main";
+import Navbar from './Navbar';
+import {NavbarItem} from './Navbar';
+
+export {
+  Footer,
+  Header,
+  Main,
+  Navbar,
+  NavbarItem
+}
