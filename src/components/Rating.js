ef058e9f0bc4f40b8f66e4e9946a3141e9e90f47
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-11-05 21:28:26 +0100

    add new Rating component

---
+++ src/components/Rating.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Rating.scss';
+
+const Rating = ({rating = 0, numReviews = '' }) => {
+  let stars = [];
+  for(let i = 1; i < 6; i ++ ){
+    stars.push(
+      <span>
+        <i
+          className={
+            rating >= i
+              ? 'fa fa-star'
+              : rating >= i - 0.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-star-o'
+          }
+        ></i>
+      </span>
+    )
+  }
+  return (
+    <div className="rating">
+      {stars}
+      <span>{numReviews + ' reviews'}</span>
+    </div>
+  );
+}
+
+export default Rating;
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
 import Footer from './Footer';
 import Header from './Header';
-import Main from "./Main";
+import Main from './Main';
 import Navbar from './Navbar';
 import {NavbarItem} from './Navbar';
-import Product from "./Product";
+import Product from './Product';
+import Rating from './Rating';
 
 export {
   Footer,
@@ -11,5 +12,6 @@ export {
   Main,
   Navbar,
   NavbarItem,
-  Product
+  Product,
+  Rating
 }
