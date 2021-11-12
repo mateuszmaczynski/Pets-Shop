5dcb5cb0a5339fbdeca9fb1d84f7996406303835
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-11-12 23:06:58 +0100

    add new action for getting product Details

--- src/actions/products.js
+++ src/actions/products.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS
 } from "../constants";
 
 export const getProductList = () => async (dispatch) => {
-  let getProductsUrl = 'https://run.mocky.io/v3/4a586095-e4c1-42c6-b129-56adbebe01f6';
+  let getProductsUrl = 'https://run.mocky.io/v3/9eed3593-c5e1-4a12-a45f-106ea86e0822';
   dispatch({
     type: PRODUCT_LIST_REQUEST,
   })
@@ -15,5 +18,22 @@ export const getProductList = () => async (dispatch) => {
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message})
+  }
+}
+
+export const getProductDetails = (productId) => async (dispatch) => {
+  dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
+  try {
+    let getProductUrl = 'https://run.mocky.io/v3/9eed3593-c5e1-4a12-a45f-106ea86e0822';
+    const { data } = await axios.get(getProductUrl);
+    console.log('data =',data);
+    const product = data.products.find(({_id}) => _id == productId);
+    console.log('product =',product);
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: product });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload: error?.response?.data?.message ? error.response.data.message : error?.message
+    })
   }
 }
