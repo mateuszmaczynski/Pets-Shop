0d8c729090d3ab2c06a4d9f45098237a133f6a1f
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-11-12 23:16:57 +0100

    SmallPetsProducts view - adding redux, components: LoadingIndicator, Message and displaying their depending on the state

--- src/views/SmallPetsProducts.js
+++ src/views/SmallPetsProducts.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import data from "../db";
-import {ProductTile} from "../components";
+import React, {useEffect, useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProductList } from '../actions/products';
+import {
+  LoadingIndicator,
+  Message,
+  ProductTile
+} from '../components';
 
 const SmallPetsProducts = () => {
-  const smallPets = data.products.filter(({category}) => (category === 'Small pet'))
+  const [smallPets, setSmallPets] = useState(false);
+  const dispatch = useDispatch();
+  const productsData = useSelector((state) => state.productList);
+  const { loading, error, products } = productsData;
+
+  useEffect( () => {
+    dispatch(getProductList())
+  },[dispatch]);
+
+  useEffect(() => {
+    if(products && products.length >= 1 ){
+      let pets = products.filter((item) => item.category ==='Small pet');
+      setSmallPets(pets)
+    }
+  }, [products])
+
   return (
-    <div className="flex-row-evenly">
-      {smallPets.map((item) => (
-        <ProductTile key={item._id} product={item} />
-      ))}
-    </div>
+    <>
+      {loading ? (
+        <LoadingIndicator />
+      ) : error ? (
+        <Message>
+          {error}
+        </Message>
+      ) : (
+        <div className='flex-row-evenly'>
+          {smallPets && smallPets.map((item) => (
+            <ProductTile key={item._id} product={item} />
+          ))}
+        </div>
+      )}
+    </>
   )
 };
 
