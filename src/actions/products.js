a5a7313d335fedc8a610b47eb21ea1e90d1cc54c
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-11-12 17:13:08 +0100

    add actions, reducers for getting all products

--- src/actions/products.js
+++ src/actions/products.js
@@ -1,15 +1,19 @@
-import Axios from 'axios';
+import axios from 'axios';
 import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS
 } from "../constants";
 
-export const getListProducts = () => async (dispatch) => {
+export const getProductList = () => async (dispatch) => {
+  let getProductsUrl = 'https://run.mocky.io/v3/4a586095-e4c1-42c6-b129-56adbebe01f6';
   dispatch({
     type: PRODUCT_LIST_REQUEST,
   })
   try {
-
+    const { data } = await axios.get(getProductsUrl);
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message})
   }
 }
