53b2d7a657fd0141822cae5a976d7d13b0283af7
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-11-14 17:30:02 +0100

    add action for serving global store (redux) for cart

---
+++ src/actions/cart.js
@@ -0,0 +1,24 @@
+import {CART_ADD_ITEM, CART_REMOVE_ITEM, PRODUCT_DETAILS_SUCCESS} from '../constants';
+
+debugger;
+export const addToCart = (productData, productId, quantity) => async (dispatch, getState) => {
+  if(productId){
+    dispatch({
+      type: CART_ADD_ITEM,
+      payload: {
+        name: productData?.name,
+        image: productData?.image,
+        price: productData?.price,
+        countInStock: productData?.countInStock,
+        id: productData?.id,
+        quantity: quantity,
+      },
+    });
+  }
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+}
+
+export const removeFromCart = (productId) => (dispatch, getState) => {
+  dispatch({ type: CART_REMOVE_ITEM, payload: productId });
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
