56f04520461bdd336a2339ac5373e4aa7650752c
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-11-14 17:33:00 +0100

    modify Header component - displaying badges next to cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 import './Header.scss';
+import {Link} from 'react-router-dom';
 import logo from '../assets/images/Logo.svg'
 import searchIcon from '../assets/icons/search.svg';
 import cart from '../assets/icons/cart.svg';
@@ -7,9 +9,11 @@ import bubbles from '../assets/icons/bubbles.svg';
 import user from '../assets/icons/user.svg';
 
 const Header = () => {
+  const cartData = useSelector((state) => state.cart);
+  const {cartItems} = cartData;
   return (
     <header className='header-container'>
-      <img src={logo} className="logo" alt="Pets online store logo" />
+      <img src={logo} className="logo" alt="Pets online store logo"/>
       <form action='#' className='search'>
         <input type='text' className='search__input' placeholder='Search product'/>
         <button className='search__button'>
@@ -18,7 +22,12 @@ const Header = () => {
       </form>
       <nav className='user-nav'>
         <div className='user-nav__box'>
-          <img src={cart} className="user-nav__icon" alt="Search icon"/>
+          <Link to="/cart">
+            <img src={cart} className="user-nav__icon" alt="Search icon"/>
+            {cartItems.length > 0 && (
+              <span className="badge">{cartItems.length}</span>
+            )}
+          </Link>
         </div>
         <div className='user-nav__box'>
           <img src={bubbles} className="user-nav__icon" alt="Search icon"/>
