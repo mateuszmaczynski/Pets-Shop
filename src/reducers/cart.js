7c06984ff83fa141cbaed55af471f78ab7c277c8
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-11-14 17:30:21 +0100

    add reducers for serving global store (redux) for cart

---
+++ src/reducers/cart.js
@@ -0,0 +1,29 @@
+import {CART_ADD_ITEM, CART_REMOVE_ITEM} from '../constants';
+
+export const cartReducer = (
+  state = {cartItems: []},
+  action
+) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload;
+      const existItem = state.cartItems.find((el) => el.id === item.id);
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((el) =>
+            el.id === existItem.id ? item : el
+          ),
+        };
+      } else {
+        return { ...state, cartItems: [...state.cartItems, item] };
+      }
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((el) => el.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
