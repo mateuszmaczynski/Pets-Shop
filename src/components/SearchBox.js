d7791281e5be7cae06dedee96e478591222009ec
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-11-17 22:41:54 +0100

    add SearchBox component

---
+++ src/components/SearchBox.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import './SearchBox.scss';
+import searchIcon from '../assets/icons/search.svg';
+import {useNavigate} from 'react-router-dom';
+
+const SearchBox = (props) => {
+  const [name, setName] = useState('');
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate(`/search/name/${name}`);
+  }
+
+  return (
+    <form className='search' onSubmit={submitHandler}>
+      <input
+        type='text'
+        name='q'
+        id='site-search'
+        minLength="3"
+        maxLength="30"
+        className='search__input'
+        placeholder='Search product'
+        onChange={(e) => setName(e.target.value)}/>
+      <button className='search__button' type='submit'>
+        <img src={searchIcon} className='search__icon' alt='Search icon'/>
+      </button>
+    </form>
+  )
+}
+
+export default SearchBox;
